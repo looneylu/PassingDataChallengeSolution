94ff7c228ea1c0afb164a518df4dcc7303c7391e
looneylu <lcarbuccia@gmail.com>
2014-07-21 00:46:39 -0400

    First Commit

---
+++ PassingDataChallengeSolution/PassingDataChallengeSolution/LRCDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  LRCDetailViewController.h
+//  PassingDataChallengeSolution
+//
+//  Created by Luis Carbuccia on 7/21/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LRCDetailViewController : UIViewController
+
+@end
